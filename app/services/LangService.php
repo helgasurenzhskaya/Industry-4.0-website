520b0b727a1ba01d9ff7a63f579c6923e8972020
olgafereal <43323630+olgafereal@users.noreply.github.com>
2018-09-30 16:50:23 +0300

    Implement skeleton for lang service

--- app/services/LangService.php
+++ app/services/LangService.php
@@ -1,19 +1,30 @@
 <?php
 class LangService 
 {
+    private $lang;
+
     function __construct($config) 
     {
+        $this->langs = [];
 
+        $config_count = count($config);
+        for ($i = 0; $i < $config_count; $i++) {
+            $lang = new Lang();
+            // TODO: проверка на существование свойств
+            $lang->setId($config[$i]['code']);
+            $lang->setTitle($config[$i]['title']);
+            $lang->setSort($i);
+            array_push($this->langs, $lang);
+        }
     }
 
-    public function getCurrent() 
+    public function getCurrent(): Lang
     {
-
+        return $this->langs[0];
     }
     
-    public function getAll() 
+    public function getAll(): array
     {
-
+        return $this->langs;
     }
-
 }
