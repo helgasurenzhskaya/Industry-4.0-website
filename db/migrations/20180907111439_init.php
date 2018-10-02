ec6cd9c4313a2c1f4d419e045b1eaaa33879c811
olgafereal <43323630+olgafereal@users.noreply.github.com>
2018-10-02 19:47:56 +0300

    Improve tables and add some data

--- db/migrations/20180907111439_init.php
+++ db/migrations/20180907111439_init.php
@@ -10,14 +10,36 @@ class Init extends AbstractMigration
         $menus->addColumn('title_uk', 'string');
         $menus->addColumn('title_en', 'string');
         $menus->addColumn('url', 'string');
-        $menus->addColumn('sort', 'integer');
+        $menus->addColumn('sort', 'integer', ['default' => 100]);
+
+        $menus->insert([
+            [
+                'title_uk' => 'Головна',
+                'title_en' => 'Home',
+                'url' => '',
+                'sort' => 10,
+            ],
+            [
+                'title_uk' => 'Інформація',
+                'title_en' => 'About',
+                'url' => '#about',
+                'sort' => 30,
+            ],
+            [
+                'title_uk' => 'Напрямки',
+                'title_en' => 'Directions',
+                'url' => '#direction',
+                'sort' => 50,
+            ],
+        ]);
+        
         $menus->create();
 
         $pages = $this->table('pages');
         $pages->addColumn('name', 'string');
-        $pages->addColumn('text_uk', 'string');
-        $pages->addColumn('text_en', 'string');
-        $pages->addColumn('sort', 'integer');
+        $pages->addColumn('text_uk', 'text');
+        $pages->addColumn('text_en', 'text');
+        $pages->addColumn('sort', 'integer', ['default' => 100]);
         $pages->create();
     }
 }
--- db/migrations/20180909155429_articles.php
+++ db/migrations/20180909155429_articles.php
@@ -6,22 +6,70 @@ class Articles extends AbstractMigration
 {
     public function change()
     {
+        $users = $this->table('users');
+        $users->addColumn('login', 'string', ['limit' => 32]);
+        $users->addColumn('name', 'string', ['limit' => 32]);
+        $users->addColumn('password', 'string', ['limit' => 32]);
+        $users->addColumn('active', 'integer', ['default' => 1]);
+        $users->addColumn('role', 'enum', ['values' => ['admin', 'manager']]);
+        $users->addIndex('login', ['unique' => true]);
+
+        $olgaId = 1;
+        $users->insert([
+            'id' => $olgaId,
+            'login' => 'olgafereal',
+            'name' => 'Olga',
+            'password' => '',
+            'role' => 'admin',
+        ]);
+        
+        $users->create();
+
         $articles = $this->table('articles');
         $articles->addColumn('author_id', 'integer');
         $articles->addColumn('title_uk', 'string');
         $articles->addColumn('title_en', 'string');
         $articles->addColumn('image', 'string', ['null' => true]);
-        $articles->addColumn('text_uk', 'string');
-        $articles->addColumn('text_en', 'string');
-        $articles->addColumn('sort', 'integer');
-        $articles->create();
+        $articles->addColumn('text_uk', 'text');
+        $articles->addColumn('text_en', 'text');
+        $articles->addColumn('sort', 'integer', ['default' => 100]);
+        $articles->addForeignKey('author_id', 'users', 'id', ['delete'=> 'CASCADE', 'update'=> 'CASCADE']);
 
-        $users = $this->table('users');
-        $users->addColumn('login', 'string');
-        $users->addColumn('name', 'string');
-        $users->addColumn('password', 'string');
-        $users->addColumn('active', 'integer');
-        $users->addColumn('role', 'enum');
-        $users->create();
+        $articles->insert([
+            [
+            'author_id' => $olgaId,
+            'title_uk' => 'Кіберфізичні системи',
+            'title_en' => 'Cyber-physical systems',
+            // 'image' => '',
+            'text_uk' => '',
+            'text_en' => '',
+            ],
+            [
+                'author_id' => $olgaId,
+                'title_uk' => 'Аналіз великих даних',
+                'title_en' => 'Big data analysis',
+                // 'image' => '',
+                'text_uk' => '',
+                'text_en' => '',
+            ],
+            [
+                'author_id' => $olgaId,
+                'title_uk' => 'Інтернет речей',
+                'title_en' => 'Internet of things',
+                // 'image' => '',
+                'text_uk' => '',
+                'text_en' => '',
+            ],
+            [
+                'author_id' => $olgaId,
+                'title_uk' => 'Штучний інтелект',
+                'title_en' => 'Artificial intelligence',
+                // 'image' => '',
+                'text_uk' => '',
+                'text_en' => '',
+            ],            
+        ]);
+
+        $articles->create();
     }
 }
