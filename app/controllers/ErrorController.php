f2bdc7ac613ce0539217d5cada793961cdc4ff6b
olgafereal <43323630+olgafereal@users.noreply.github.com>
2018-10-14 20:07:50 +0300

    Show only conroller view and se error code

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -1,16 +1,19 @@
 <?php
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Mvc\View;
 
 class ErrorController extends Controller 
 {
     public function show404Action() 
     {
-        
+        $this->view->setRenderLevel(View::LEVEL_AFTER_TEMPLATE);
+        $this->response->setStatusCode(404); 
     }
 
     public function show500Action() 
     {
-        
+        $this->view->setRenderLevel(View::LEVEL_AFTER_TEMPLATE);
+        $this->response->setStatusCode(500); 
     }
 }
