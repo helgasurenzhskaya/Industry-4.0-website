db3260387717442080e0e59812fb6fa3ce3f895b
olgafereal <43323630+olgafereal@users.noreply.github.com>
2018-11-18 01:28:08 +0200

    Article edit

--- app/backend/forms/ArticleAddEditForm.php
+++ app/backend/forms/ArticleAddEditForm.php
@@ -2,19 +2,56 @@
 namespace Backend\Forms;
 
 use Phalcon\Forms\Form;
-// use Phalcon\Forms\Element\Text;
-// use Phalcon\Forms\Element\TextArea;
-// use Phalcon\Forms\Element\Password;
-// use Phalcon\Forms\Element\Hidden;
-// use Phalcon\Validation\Validator\PresenceOf;
-// use Phalcon\Validation\Validator\StringLength;
-// use Phalcon\Validation\Validator\Identical;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\TextArea;
+use Phalcon\Forms\Element\Password;
+use Phalcon\Forms\Element\Hidden;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\StringLength;
+use Phalcon\Validation\Validator\Identical;
 
 class ArticleAddEditForm extends Form
 {
 
     public function initialize()
     {
+        $this->setAction($this->url->get([
+            'for' => 'backend/page/item_action',
+            'action' => 'edit',
+            'page_id' => $this->getEntity()->getId(),
+        ]));
 
+        $name = new Text('title');
+        $name->setLabel('title');
+        $name->addValidators([
+            new PresenceOf([
+                'message' => 'Please fill "Title" field.',
+            ]),
+            new StringLength([
+                'max' => 255,
+                'min' => 2,
+                'messageMaximum' => 'Can not be longer than 255 characters',
+                'messageMinimum' => 'Can not be shorter than 2 characters',
+            ]),
+        ]);
+
+        $this->add($name);
+
+        $langs = $this->lang->getAll();
+        foreach ($langs as $lang) {
+            $text = new TextArea('text_' . $lang->getId());
+            $text->setLabel('Text ' . $lang->getId());
+            $text->addValidators([
+                new PresenceOf([
+                    'message' => 'Please fill "Text ' . $lang->getId() . '" field.',
+                ]),
+                new StringLength([
+                    'max' => 65000,
+                    'messageMaximum' => 'Can not be longer than 65000 characters',
+                ]),
+            ]);
+
+            $this->add($text);
+        }
     }
 }
